7323366de597045e0ef8dbedceb1fb891e9e4320
heardMan <markyheard@gmail.com>
2019-02-17 22:35:44 -0800

    added more styling

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
 var friends = [
     {
         "name": "James",
-        "photo": "#",
+        "photo": "http://o.aolcdn.com/hss/storage/midas/6174d178db898d9be89efd14649aa3c4/202316821/badass+celebrity+selfies+7.jpeg",
         "scores": [5,3,4,4,1,5,3,4,4,1]
     },
     {
         "name": "Henry",
-        "photo": "#",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFj8fOeiRqbtxbYW2kBHNVC8ZtRBNiLbrCeLYKqhtyYCr0k4UGDA",
         "scores": [4,3,2,4,3,1,3,2,2,1]
     },
     {
         "name": "Karen",
-        "photo": "#",
+        "photo": "https://cdn.lolwot.com/wp-content/uploads/2016/02/20-leaked-celebrity-selfies-youve-never-seen-before-1.jpg",
         "scores": [4,5,2,5,3,5,3,2,5,1]
     },
     {
         "name": "Steevi",
-        "photo": "#",
+        "photo": "https://i0.wp.com/www.feedpuzzle.com/wp-content/uploads/2016/04/nomakeup-celebrity-selfies-celebrities-without-makeup-1459593669lpc84.jpg?fit=480%2C480&ssl=1",
         "scores": [4,1,2,5,3,2,4,1,3,4]
     },
     {
         "name": "Phinn",
-        "photo": "#",
+        "photo": "http://cdn.oboxeditions.com/sites/prod/files/styles/largehd/public/gallery/sometime-pop-doesnt-rime-hot-242543.png",
         "scores": [2,5,3,3,2,5,2,3,5,5]
     },
     {
         "name": "Aubrey",
-        "photo": "#",
+        "photo": "https://i.pinimg.com/236x/a9/03/5e/a9035ec93639bf6a827cdebb1397c9fc--celebrity-selfies-selena-gomez.jpg",
         "scores": [5,5,4,5,5,5,5,4,4,4]
     },
 ];
